66934fe6003b24f9718bfb15fae6781114014d8d
nucl <nucl@alpenjodel.de>
2016-06-13 21:16:44 +0200

    -

--- www/js/controlling.js
+++ www/js/controlling.js
@@ -2,24 +2,26 @@
 var controlling = new Controlling();
 
 function Controlling() {
-    this.angle = 0.0;
-    this.lift = 0.0;
+    this.forward = false;
+    this.backward = false;
+    this.strafeLeft = false;
+    this.strafeRight = false;
 }
 
 Controlling.prototype.handleKeydownEvent = function(e) {
     var code = e.keyCode;
     switch (code) {
-        case 87:
-            this.lift = 1;
+        case 87: //'w':
+            this.forward = true;
             break;
-        case 83:
-            this.lift = -0.001;
+        case 65: //'a':
+            this.strafeLeft = true;
             break;
-        case 65:
-            this.angle = 1;
+        case 83: //'s':
+            this.backward = true;
             break;
-        case 68:
-            this.angle = -1;
+        case 68: //'d':
+            this.strafeRight = true;
             break;
     	
         default:
@@ -29,17 +31,17 @@ Controlling.prototype.handleKeydownEvent = function(e) {
 Controlling.prototype.handleKeyupEvent = function(e) {
     var code = e.keyCode;
     switch (code) {
-        case 87:
-            this.lift = 0;
+        case 87: //'w':
+            this.forward = false;
             break;
-        case 83:
-            this.lift = 0;
+        case 65: //'a':
+            this.strafeLeft = false;
             break;
-        case 65:
-            this.angle = 0;
+        case 83: //'s':
+            this.backward = false;
             break;
-        case 68:
-            this.angle = 0;
+        case 68: //'d':
+            this.strafeRight = false;
             break;
 
         default:
