b99958b179a23673b4cf4c0afbdc242ae6fbc93b
Threlgor <johannes.oberhofer@googlemail.com>
2017-02-25 21:20:38 +0100

    sa

---
+++ pixi/src/main.test.js
@@ -0,0 +1,15 @@
+import { Engine, World, Bodies, Render } from 'matter-js'
+
+test('3 equal 3', () => {
+	const engine = Engine.create()
+	const playerPhysics = Bodies.circle(80, 80, 20, { restitution: 0.01, frictionAir: 0.5 })
+	playerPhysics.force.x = 1
+	World.add(engine.world, playerPhysics)
+
+	console.log(playerPhysics.position)
+	Engine.update(engine, 16.666)
+	console.log(playerPhysics.position)
+	Engine.update(engine, 16.666)
+	console.log(playerPhysics.position)
+
+})
