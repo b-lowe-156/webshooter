2cceb816912a3dce089b079c7b8158ada061b0e1
Threlgor <johannes.oberhofer@googlemail.com>
2017-07-31 22:57:21 +0200

    hg

--- heli/future2.js
+++ heli/future2.js
@@ -9,36 +9,50 @@ const pool = new Pool({
 	port: 5432,
 })
 
+const query = (client, query) => Future((reject, resolve) => {
+	client.query(query, (err, result) => {
+		if (err) {
+			client.errorOccured = true
+			reject('error in query')
+		} else {
+			resolve(result.rows[0])
+		}
+	})
+	//Cancellation:
+	return () => { }
+})
+
 const openConnection = () => Future((reject, resolve) => {
+	console.log('open connection')
 	pool.connect((err, client, release) => {
 		if (err) {
 			reject('Error acquiring client')
 		}
-		resolve(client)
+		client.query('begin', (err, result) => {
+			console.log('begin transaction')
+			if (err) {
+				reject('error in beginning the transaction', err)
+			}
+			resolve(client)
+		})
 	})
 	// Cancellation:
 	return () => release()
 })
 
 const closeConnection = client => Future((reject, resolve) => {
-	console.log('close connection')
-	client.release()
-	resolve()
-	//Cancellation:
-	return () => release()
-})
-
-const query = (client, query) => Future((reject, resolve) => {
-	client.query(query, (err, result) => {
+	client.query('commit', (err, result) => {
 		if (err) {
-			reject('error in query', err)
+			console.log('error in commiting transaction')
+		} else {
+			console.log('transaction commit done')
+			client.release()
+			resolve()
 		}
-		resolve(result.rows[0])
 	})
 	//Cancellation:
-	return () => { }
+	return () => client.release()
 })
-
 
 const withTransaction = Future.hook(
 	openConnection(),
@@ -46,18 +60,18 @@ const withTransaction = Future.hook(
 )
 
 withTransaction(
-	client =>
-		query(client, 'SELECT * FROM article')
+	tx =>
+		query(tx, 'SELECT * FROM article')
 		.map(e => e.name)
 		.map(e => e)
-		.chain(n => query(client, 'SELECT * FROM article'))
+		.chain(n => query(tx, 'SELECT * FROM article'))
 		.map(e => e)
 )
 .fork(
 	err => {
-		console.log('error', err)
+		console.log('error')
 	},
 	client => {
-		console.log('end', client)
+		console.log('end')
 	}
 )
