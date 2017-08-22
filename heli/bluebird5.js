1c0e69c5513c813665a71afb137fd96af61dbd0a
Threlgor <johannes.oberhofer@googlemail.com>
2017-08-22 22:47:58 +0200

    sdf

---
+++ heli/bluebird5.js
@@ -0,0 +1,44 @@
+const pg = require("pg")
+const Pool = require("pg").Pool
+
+const Promise = require("bluebird")
+Promise.promisifyAll(pg, { multiArgs: false })
+Promise.promisifyAll(Pool, { multiArgs: true })
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'start123',
+    port: 5432,
+    Promise: require("bluebird")
+})
+
+const getDbConnection = () =>
+    pool.connectAsync()
+    .spread(client => client)
+    .disposer(client => {
+        client.release()
+    })
+
+const withTransaction = queryChain =>
+    Promise.using(getDbConnection(),
+        connection =>
+            Promise.try(() =>
+                connection.queryAsync('BEGIN')
+                .then(() => queryChain(connection))
+            )
+            .then(res => connection.queryAsync('COMMIT').return(res))
+            .catch(err => connection.queryAsync('ROLLBACK').return(err))
+    )
+
+withTransaction(
+    tx =>
+        tx.queryAsync('SELECT * FROM article')
+        .then(result => {
+            console.log('result', result)
+        })
+        .catch(err => {
+            console.log('err', err)
+        })
+)
