02074b5067c13926a284c1f40c9caf9773a8d4f3
Threlgor <johannes.oberhofer@googlemail.com>
2017-08-28 23:51:47 +0200

    d

--- heli/or.test.js
+++ heli/or.test.js
@@ -1,10 +1,36 @@
 
-const articleDef = [
-  { name: 'id', type: 'pk' },
-  { name: 'version', type: 'version' },
-  { name: 'name', type: 'text' },
-  { name: 'lastname', type: 'text' },
-]
+const insertStatement = definition => entity => {
+  const columsToInsert = definition.columns.filter(c => c.type !== 'pk' && c.type !== 'version')
+  const query = `insert into ${
+    definition.tableName
+  } (version, ${
+    columsToInsert.map(n => n.name)
+    .join(', ')
+  }) VALUES ($1, ${
+    columsToInsert.map((n, i) => '$' + (i + 2))
+    .join(', ')
+  })`
+  const params = [1].concat(columsToInsert.map(n => entity[n.name]))
+  return {
+    query,
+    params,
+  }
+}
+
+const selectQuery = definition => `select ${
+  definition.columns.map(n => n.name)
+  .join(', ')
+} from ${ definition.tableName }`
+
+const articleDef = {
+  tableName: 'article',
+  columns: [
+    { name: 'id', type: 'pk' },
+    { name: 'version', type: 'version' },
+    { name: 'name', type: 'text' },
+    { name: 'lastname', type: 'text' },
+  ],
+}
 
 const article = {
   id: 5,
@@ -13,32 +39,28 @@ const article = {
   lastname: 'rar',
 }
 
-const insertArticeStatement = article => {
-  const columsToInsert = articleDef.filter(n => n.type !== 'pk' && n.type !== 'version')
-  const query = `insert into article (${
-    columsToInsert.map(n => n.name)
-    .join(', ')
-  }) VALUES (${
-    columsToInsert.map((n, i) => '$' + (i + 1))
-    .join(', ')
-  })`
-  const params = [article.name, article.lastname]
-  return { query, params }
-}
+const insertArticeStatement = insertStatement(articleDef)
 
-test('', () => {
+test('select statement', () => {
+  expect(selectQuery(articleDef))
+  .toBe('select id, version, name, lastname from article')
+})
+
+test('insert statement', () => {
   const data = insertArticeStatement(article)
   expect(data.query)
-  .toBe('insert into article (name, lastname) VALUES ($1, $2)')
+  .toBe('insert into article (version, name, lastname) VALUES ($1, $2, $3)')
 })
 
-test('', () => {
+test('insert params', () => {
   const data = insertArticeStatement(article)
-  expect(data.params.length)
-  .toBe(2)
+  expect(data.params.length).toBe(3)
+  expect(data.params[0]).toBe(1)
+  expect(data.params[1]).toBe('dudu')
+  expect(data.params[2]).toBe('rar')
 })
 
-test('', () => {
+test('update statement', () => {
   expect('update article set name = $2, lastname = $3 where id = $1')
   .toBe('update article set name = $2, lastname = $3 where id = $1')
 })
