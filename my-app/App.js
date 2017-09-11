b1018ee416067aae2e9197871005824630f02c68
Threlgor <johannes.oberhofer@googlemail.com>
2017-09-11 22:46:32 +0200

    cvb

--- my-app/App.js
+++ my-app/App.js
@@ -1,38 +1,49 @@
-import React, { PureComponent } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+import React, { PureComponent } from 'react'
+import { View, Text, StyleSheet, ToolbarAndroid } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view'
 
-const FirstRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
-const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
+const FirstRoute = () => <View style={[styles.container, { backgroundColor: '#ffffff' }]} />
+const SecondRoute = () => <View style={[styles.container, { backgroundColor: '#ffffff' }]} />
+const NachrichtenRoute = () => <View style={[styles.container, { backgroundColor: '#ffffff' }]} />
 
 export default class TabViewExample extends PureComponent {
   state = {
     index: 0,
     routes: [
-      { key: '1', title: 'First' },
-      { key: '2', title: 'Second' },
+      { key: '1', title: 'Dashboard', icon: 'md-restaurant' },
+      { key: '2', title: 'Article', icon: 'md-bicycle' },
+      { key: '3', title: 'Nachrichten', icon: 'md-bicycle' },
     ],
-  };
+  }
 
-  _handleIndexChange = index => this.setState({ index });
+  _handleIndexChange = index => this.setState({ index })
 
-  _renderHeader = props => <TabBar {...props} />;
+  _renderIcon = ({ route }) => {
+      return <Ionicons name={route.icon} size={24} color="black" />;
+    }
+
+  _renderHeader = props => <TabBar {...props} renderIcon={this._renderIcon} style={{ backgroundColor: '#ffffff' }} labelStyle={{ color: '#ff00ff' }} />
 
   _renderScene = SceneMap({
     '1': FirstRoute,
     '2': SecondRoute,
-  });
+    '3': NachrichtenRoute,
+  })
 
   render() {
     return (
-      <TabViewAnimated
-        style={styles.container}
-        navigationState={this.state}
-        renderScene={this._renderScene}
-        renderHeader={this._renderHeader}
-        onIndexChange={this._handleIndexChange}
-      />
-    );
+      <View style={styles.container}>
+        <View style={{ height: 24 }} />
+        <TabViewAnimated
+          style={styles.container}
+          navigationState={this.state}
+          renderScene={this._renderScene}
+          renderHeader={this._renderHeader}
+          onIndexChange={this._handleIndexChange}
+        />
+      </View>
+    )
   }
 }
 
@@ -40,4 +51,4 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-});
+})
