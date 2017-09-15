f33dd7500495fa016b13f61d457ab57fe12b01a0
Threlgor <johannes.oberhofer@googlemail.com>
2017-09-15 22:24:54 +0200

    sad

--- my-app/App.js
+++ my-app/App.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import { View, Text, StyleSheet, ToolbarAndroid } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
-import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view'
+import { TabViewAnimated, TabViewPagerAndroid, TabBar, SceneMap } from 'react-native-tab-view'
 
 const FirstRoute = () => <View style={[styles.container, { backgroundColor: '#ffffff' }]} />
 const SecondRoute = () => <View style={[styles.container, { backgroundColor: '#ffffff' }]} />
@@ -11,16 +11,16 @@ export default class TabViewExample extends PureComponent {
   state = {
     index: 0,
     routes: [
-      { key: '1', title: 'Dashboard', icon: 'md-restaurant' },
-      { key: '2', title: 'Article', icon: 'md-bicycle' },
-      { key: '3', title: 'Nachrichten', icon: 'md-bicycle' },
+      { key: '1', icon: 'md-restaurant' },
+      { key: '2', icon: 'md-bicycle' },
+      { key: '3', icon: 'md-bicycle' },
     ],
   }
 
   _handleIndexChange = index => this.setState({ index })
 
   _renderIcon = ({ route }) => {
-      return <Ionicons name={route.icon} size={24} color="black" />;
+      return <Ionicons name={route.icon} size={24} color="blue" />;
     }
 
   _renderHeader = props => <TabBar {...props} renderIcon={this._renderIcon} style={{ backgroundColor: '#ffffff' }} labelStyle={{ color: '#ff00ff' }} />
@@ -33,8 +33,14 @@ export default class TabViewExample extends PureComponent {
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={{ height: 24 }} />
+      <View style={{flex: 1}}>
+        <View style={{flex: 1,  height: 26 }} />
+        <View style={{flex: 3, backgroundColor: 'powderblue'}}>
+          <Text style={{flex: 2,  height: 26 }}>Hello world!</Text>
+        </View>
+        <View style={{flex: 4, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 5, backgroundColor: 'steelblue'}} />
+        {/*
         <TabViewAnimated
           style={styles.container}
           navigationState={this.state}
@@ -42,6 +48,7 @@ export default class TabViewExample extends PureComponent {
           renderHeader={this._renderHeader}
           onIndexChange={this._handleIndexChange}
         />
+        */}
       </View>
     )
   }
